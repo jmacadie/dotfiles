0aa56bdd0b9bc72222b6059fbafa85e1fe02f7f3
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2023-09-20 10:42:41 +0100

    Added formatting for pyttg docstrings
    We're using a format specific so it will work with Sphinx (according to
    Joel). So write my own rules to format the pop-up text nicely

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -11,13 +11,24 @@ return {
 				["cmp.entry.get_documentation"] = true,
 			},
 		},
+		markdown = {
+			highlights = {
+				["^%s*(Args:)"] = "@text.title",
+				["^%s*(Returns:)"] = "@text.title",
+				["^%s*(Output [S|s]tatements)"] = "@text.title",
+				["^%s*([%l_]+)%s+%(.+%)%s?:"] = "@parameter",
+				["%((.+)%)%s?:"] = "@type",
+				["%s*(self)[%s|%.]"] = "@variable.builtin",
+				["%s*self%.([%l_]+)"] = "@field",
+			},
+		},
 		-- you can enable a preset for easier configuration
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 	},
 	dependencies = {
