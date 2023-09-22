dcb957be60d0a394254137ff98421f43ee41e69e
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2023-09-22 09:42:53 +0100

    Finish version check script
    Pretty happy with this now. Will run at start-up each time

--- zsh/check_versions.sh
+++ zsh/check_versions.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 function run_check() {
-  if [[ $2 != $3 ]]; then
-    echo -e
-    echo "$1 is outdated!"
-    echo "-------------------"
-    echo "Installed version is $2"
-    echo "Latest released version is $3, ($4)"
-    echo "See: $5"
-  fi
-}
+  local red="\e[31m"
+  local green="\e[32m"
+  local yellow="\e[33m"
+  local blue="\e[34m"
+  local grey="\e[90m"
+  local reset="\e[0m"
 
-function api_url() {
-  echo $1 | sed "s#://#://api.#" | sed "s#github\.com#github.com/repos#"
+  if [[ $2 != $3 ]]; then
+    echo -e "$yellow$1$reset: $yellow$2$reset -> $blue$3 ($4)$reset ❌ - $grey$5$reset"
+  else
+    echo -e "$green$1$reset: $grey$2 ($4)$reset ✅"
+  fi
 }
 
 function api() {
@@ -28,6 +28,8 @@ function version() {
 function date() {
   echo $(echo "$1" | jq -r '.published_at | sub("T.*$"; "")')
 }
+
+echo "Checking software is up to date..."
 
 # Neovim
 url="https://github.com/neovim/neovim/releases"
@@ -74,5 +76,37 @@ url="https://github.com/eza-community/eza/releases"
 details=$(api "$url" "^v[0-9]")
 latest=$(version "$details")
 latest_date=$(date "$details")
-installed=$(eza --version | rg "^v[0-9]" | sed "s/ [\+git]//")
+installed=$(eza --version | rg "^v[0-9]" | sed "s/ \[+git\]//")
 run_check "eza" $installed $latest $latest_date $url
+
+# delta
+url="https://github.com/dandavison/delta/releases"
+details=$(api "$url" "^[0-9]")
+latest=$(version "$details")
+latest_date=$(date "$details")
+installed=$(delta --version | rg "^delta [0-9]" | sed "s/^delta //")
+run_check "delta" $installed $latest $latest_date $url
+
+# fzf
+url="https://github.com/junegunn/fzf/releases"
+details=$(api "$url" "^[0-9]")
+latest=$(version "$details")
+latest_date=$(date "$details")
+installed=$(fzf --version | rg "^[0-9]" | sed "s/ ([0-9a-f]\+)$//")
+run_check "fzf" $installed $latest $latest_date $url
+
+# zoxide
+url="https://github.com/ajeetdsouza/zoxide/releases"
+details=$(api "$url" "^v[0-9]")
+latest=$(version "$details")
+latest_date=$(date "$details")
+installed=$(zoxide --version | rg "^zoxide [0-9]" | sed "s/^zoxide /v/")
+run_check "zoxide" $installed $latest $latest_date $url
+
+# bat
+url="https://github.com/sharkdp/bat/releases"
+details=$(api "$url" "^v[0-9]")
+latest=$(version "$details")
+latest_date=$(date "$details")
+installed=$(bat --version | rg "^bat [0-9]" | sed "s/^bat /v/" | sed "s/ ([0-9a-f]\+)$//")
+run_check "bat" $installed $latest $latest_date $url
