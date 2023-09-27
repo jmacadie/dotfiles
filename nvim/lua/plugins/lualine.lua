82fc68d67ce5e40bfcc13bbd18a0accfdec3a648
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2023-09-27 20:52:10 +0100

    Change statusbar on Insert
    I prefer having it less subtle!

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,12 +2,17 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	-- See `:help lualine.txt`
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = "onedark",
-			component_separators = "|",
-			section_separators = "",
-		},
-	},
+	config = function()
+		local custom_theme = require("lualine.themes.nordfox")
+		custom_theme.insert.c = { bg = "#b1d196", fg = "#3b4252" }
+
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = custom_theme,
+				component_separators = "|",
+				section_separators = "",
+			},
+		})
+	end,
 }
