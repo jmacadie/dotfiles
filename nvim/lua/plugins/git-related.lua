beaecfd59674cf67e0b3b6fe050ef975437fe38b
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2023-10-09 10:46:13 +0100

    Shortcut for git diff
    Using vim fugative. Hardcoded to diff against the development branch,
    which is what I want for now

--- nvim/lua/plugins/git-related.lua
+++ nvim/lua/plugins/git-related.lua
@@ -1,4 +1,6 @@
 -- Git related plugins
+vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff development<cr>", { desc = "Vim Fugative: [G]it [D]iff development" })
+
 return {
 	"tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
