75f7b5722078253e0a21e542fc848268ae3c417d
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2024-06-06 09:18:17 +0100

    More updates to the SSH script
    It wasn't working consistently across my Windows and Linux machines so
    trying a new variant

--- zsh/.zsh/ssh.sh
+++ zsh/.zsh/ssh.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-function is_agent_running {
-  # Check if SSH_AGENT_PID environment variable is set
-  if [[ -z "$SSH_AGENT_PID" ]]; then
-      return 1
-  fi
-
-  # Check if the process with SSH_AGENT_PID is running
-  if ps -p $SSH_AGENT_PID > /dev/null; then
-      return 0
-  else
-      return 1
-  fi
-}
-
-if [[ ! is_agent_running ]]; then
+ps -ef | grep -v grep | grep ssh-agent | grep $(whoami | head -c 8) > /dev/null
+if [[ $? == 1 ]]; then
   echo "SSH agent not running. Starting..."
   eval $(ssh-agent -s)
   ssh-add
