a0122180d96a72a340f2cc3398628582fd2b50ca
James MacAdie <j.macadie@ttg.co.uk>
2024-09-19 12:20:25 +0100

    Open Vim Fugitive in vertical split
    On wide screen monitors, vertical is my preference, so make that the
    default. Have a shortcut for horizontal too, in case I want it for
    whatever reason

--- nvim/lua/plugins/git-related.lua
+++ nvim/lua/plugins/git-related.lua
@@ -1,6 +1,7 @@
 -- Git related plugins
 vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff development<cr>", { desc = "Vim Fugative: [G]it [D]iff development" })
-vim.keymap.set("n", "<leader>gg", "<cmd>Git<cr>", { desc = "Vim Fugative: [G]it" })
+vim.keymap.set("n", "<leader>gg", "<cmd>vertical Git<cr>", { desc = "Vim Fugative: [G]it" })
+vim.keymap.set("n", "<leader>gh", "<cmd>Git<cr>", { desc = "Vim Fugative: [G]it in [H]orizontal split" })
 
 return {
 	"tpope/vim-fugitive",
