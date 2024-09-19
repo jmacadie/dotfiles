df560d5af3d6a10e2513091b1472a4b697562dfc
James MacAdie <j.macadie@ttg.co.uk>
2024-09-19 11:57:23 +0100

    Git updates

--- nvim/lua/plugins/git-related.lua
+++ nvim/lua/plugins/git-related.lua
@@ -1,5 +1,6 @@
 -- Git related plugins
 vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff development<cr>", { desc = "Vim Fugative: [G]it [D]iff development" })
+vim.keymap.set("n", "<leader>gg", "<cmd>Git<cr>", { desc = "Vim Fugative: [G]it" })
 
 return {
 	"tpope/vim-fugitive",
