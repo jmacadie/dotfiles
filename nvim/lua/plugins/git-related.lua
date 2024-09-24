2323bf8a1b5dc6fe2fdd360628a60581e6696a9c
James MacAdie <j.macadie@ttg.co.uk>
2024-09-24 17:00:32 +0100

    Git keymaps
    Vim Fugitive git blame is better, so use that
    
    Also add a key map for the Vim Fugitive git log, which is also top stuff

--- nvim/lua/plugins/git-related.lua
+++ nvim/lua/plugins/git-related.lua
@@ -1,7 +1,9 @@
 -- Git related plugins
 vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff development<cr>", { desc = "Vim Fugative: [G]it [D]iff development" })
 vim.keymap.set("n", "<leader>gg", "<cmd>vertical Git<cr>", { desc = "Vim Fugative: [G]it" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "Vim Fugative: [G]it [B]lame" })
 vim.keymap.set("n", "<leader>gh", "<cmd>Git<cr>", { desc = "Vim Fugative: [G]it in [H]orizontal split" })
+vim.keymap.set("n", "<leader>gl", "<cmd>vertical Git log<cr>", { desc = "Vim Fugative: [G]it [L]og" })
 
 return {
 	"tpope/vim-fugitive",
