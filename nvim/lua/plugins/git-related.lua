8b5df8745b45120ebf25d08462b9d7580d441a61
James MacAdie <j.macadie@ttg.co.uk>
2024-10-21 16:36:55 +0100

    More vim fugitive shortcuts
    I wanted to make the diff command more general and allow me to enter any
    refspec I wanted

--- nvim/lua/plugins/git-related.lua
+++ nvim/lua/plugins/git-related.lua
@@ -1,10 +1,11 @@
 -- Git related plugins
-vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiff development<cr>", { desc = "Vim Fugative: [G]it [D]iff development" })
+vim.keymap.set("n", "<leader>gd", ":Gvdiff ", { desc = "Vim Fugative: [G]it [D]iff (provide your own refspec)" })
 vim.keymap.set("n", "<leader>gg", "<cmd>vertical Git<cr>", { desc = "Vim Fugative: [G]it" })
 vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Vim Fugative: [G]it [W]rite 'Stage whole file'" })
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "Vim Fugative: [G]it [B]lame" })
 vim.keymap.set("n", "<leader>gh", "<cmd>Git<cr>", { desc = "Vim Fugative: [G]it in [H]orizontal split" })
 vim.keymap.set("n", "<leader>gl", "<cmd>vertical Git log<cr>", { desc = "Vim Fugative: [G]it [L]og" })
+vim.keymap.set("n", "<leader>gcl", "<cmd>vertical Git log %<cr>", { desc = "Vim Fugative: [G]it [C]urrent file [L]og" })
 vim.keymap.set("n", "<leader>gm", "<cmd>Gvdiffsplit!<cr>", { desc = "Vim Fugative: [G]it 3 Way [M]erge split" })
 vim.keymap.set("n", "<leader>ga", "<cmd>diffget //2<cr>", { desc = "[G]it: Get changes from <<<<< HEAD" })
 vim.keymap.set("n", "<leader>go", "<cmd>diffget //3<cr>", { desc = "[G]it: Get changes from Target >>>>>" })
