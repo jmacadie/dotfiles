c1eb184359e6b929bcc2d535fad616826efd5383
James MacAdie <j.macadie@ttg.co.uk>
2025-01-06 09:15:45 +0000

    Telescope Hierarchy plugin

---
+++ nvim/lua/plugins/telescope-hierarchy.lua
@@ -0,0 +1,33 @@
+return {
+	"jmacadie/telescope-hierarchy.nvim",
+	-- dir = "/home/james/plugins/telescope-hierarchy.nvim",
+	dependencies = { -- note how they're inverted to above example
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
+	},
+	keys = {
+		{ -- lazy style key map
+			"<leader>si",
+			"<cmd>Telescope hierarchy<cr>",
+			desc = "[S]earch [I]ncoming Calls",
+		},
+	},
+	opts = {
+		-- don't use `defaults = { }` here, do this in the main telescope spec
+		extensions = {
+			hierarchy = {
+				-- telescope-hierarchy.nvim config, see below
+			},
+			-- no other extensions here, they can have their own spec too
+		},
+	},
+	config = function(_, opts)
+		-- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+		-- configs for us. We won't use data, as everything is in it's own namespace (telescope
+		-- defaults, as well as each extension).
+		require("telescope").setup(opts)
+		require("telescope").load_extension("hierarchy")
+	end,
+}
