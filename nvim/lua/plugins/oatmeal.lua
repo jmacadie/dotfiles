604ecf025b60e16559bbedff51df9898125feff9
James MacAdie <j.macadie@ttg.co.uk>
2025-01-24 16:05:46 +0000

    Oatmeal
    Adding this as it looks useful

---
+++ nvim/lua/plugins/oatmeal.lua
@@ -0,0 +1,11 @@
+return {
+	"dustinblackman/oatmeal.nvim",
+	cmd = { "Oatmeal" },
+	keys = {
+		{ "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+	},
+	opts = {
+		backend = "openai",
+		model = "gpt-4o",
+	},
+}
