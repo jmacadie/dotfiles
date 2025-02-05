a80865bed74be50d788ddfd4113caa9e98a05180
James MacAdie <j.macadie@ttg.co.uk>
2025-02-05 15:40:09 +0000

    Update PR reviewer
    Now the code will accommodate moved files. I also don't bother trying to do the
    diff for added files, since this always generates an error due to the file not
    existing in the tree in the upstream branch

--- nvim/lua/custom/qf_diff.lua
+++ nvim/lua/custom/qf_diff.lua
@@ -12,6 +12,17 @@ local STATUS_MAP = {
 	["X"] = "UNKNOWN",
 }
 
+---@class FileDiffStatus
+---@field filename string The path and filename of the file. If the file has been moved this will be the file *after* the move
+---@field source_filename string | nil If the file was moved, what was the source filename
+---@field status string
+---@field lines_added integer
+---@field lines_deleted integer
+
+---Convert the diff between the current worktree and an arbitrary refspec to a table of
+---files that have been modified, the type of change, and the number of lines changed
+---@param remote_branch string | nil
+---@return FileDiffStatus[]
 local function get_diff_files(remote_branch)
 	local result = {}
 
@@ -25,23 +36,54 @@ local function get_diff_files(remote_branch)
 
 	local name_status_map = {}
 	for _, line in ipairs(name_status) do
-		local status, filename = line:match("^(%S+)%s+(.+)$")
-		if status and filename then
-			name_status_map[filename] = STATUS_MAP[status] or "?"
+		if line:sub(1, 1) == "R" then
+			local status, pcnt, filename, rename_filename = line:match("^(%u)(%d*)%s+(%S+)%s+(%S+)$")
+			local pcnt_val = tonumber(pcnt)
+			local mapped_status = string.format("%s (%d%%)", STATUS_MAP[status], pcnt_val)
+			if status and filename then
+				name_status_map[rename_filename] = { status = mapped_status, from = filename }
+			end
+		else
+			local status, filename = line:match("^(%u)%s+(%S+)$")
+			local mapped_status = STATUS_MAP[status] or "?"
+			if status and filename then
+				name_status_map[filename] = { status = mapped_status }
+			end
 		end
 	end
 
 	for _, line in ipairs(numstat) do
+		---@type string | nil, string | nil, string | nil
 		local lines_added, lines_deleted, filename = line:match("^(%d+)%s+(%d+)%s+(.+)$")
 		if lines_added and lines_deleted and filename then
-			table.insert(result, {
-				filename = filename,
-				status = name_status_map[filename] or "?", -- Default status to "?" if not found
-				lines_added = tonumber(lines_added),
-				lines_deleted = tonumber(lines_deleted),
-			})
+			local added = tonumber(lines_added)
+			local deleted = tonumber(lines_deleted)
+			---@cast filename string
+			local prefix, part_1, part_2, suffix = filename:match("^([^{]*){(%S+) => (%S+)}(%S*)")
+			if part_1 or part_2 then
+				local from_fname = string.format("%s%s%s", prefix, part_1, suffix)
+				local to_fname = string.format("%s%s%s", prefix, part_2, suffix)
+				table.insert(result, {
+					filename = to_fname,
+					source_filename = from_fname,
+					display = filename,
+					status = name_status_map[to_fname].status or "?", -- Default status to "?" if not found
+					lines_added = added,
+					lines_deleted = deleted,
+				})
+			else
+				table.insert(result, {
+					filename = filename,
+					source_filename = nil,
+					display = filename,
+					status = name_status_map[filename].status or "?", -- Default status to "?" if not found
+					lines_added = added,
+					lines_deleted = deleted,
+				})
+			end
 		end
 	end
+	-- vim.notify(vim.inspect(result), vim.log.levels.INFO)
 
 	return result
 end
@@ -50,12 +92,12 @@ local function format_quickfix(info)
 	local result = {}
 
 	for _, item in ipairs(vim.fn.getqflist({ id = info.id, items = 0 }).items) do
-		local filename = item.bufnr and vim.fn.bufname(item.bufnr) or ""
 		local extra = item.user_data
+		local display = extra["display"] or ""
 		local status = extra["status"] or ""
 		local added = extra["lines_added"] or ""
 		local deleted = extra["lines_deleted"] or ""
-		table.insert(result, string.format("%s | +%d -%d | %s", filename, added, deleted, status))
+		table.insert(result, string.format("%s | +%d -%d | %s", display, added, deleted, status))
 	end
 
 	return result
@@ -68,6 +110,8 @@ local function populate_quickfix(parsed_files)
 		table.insert(items, {
 			filename = item.filename,
 			user_data = {
+				display = item.display,
+				source = item.source_filename,
 				status = item.status,
 				lines_added = item.lines_added,
 				lines_deleted = item.lines_deleted,
@@ -129,7 +173,20 @@ local function cycle_qf_backward()
 end
 
 local function diff_current_file()
-	vim.cmd("Gvdiff @{u}")
+	local qf_idx = vim.fn.getqflist({ idx = 0 }).idx
+	local qf_list = vim.fn.getqflist()
+	local data = qf_list[qf_idx].user_data
+
+	if data.status == "ADDED" then
+		return
+	end
+
+	local source = data.source
+	if source then
+		vim.cmd(string.format("Gvdiffsplit @{u}:%s", source))
+	else
+		vim.cmd("Gvdiffsplit @{u}")
+	end
 	vim.cmd("wincmd p")
 end
 
