6b52fc3072834b76acc63a3881aa7eebc6307fb9
James MacAdie <j.macadie@ttg.co.uk>
2025-03-07 12:40:38 +0000

    Custom JSON formatter
    I want a blank line at the end and `trim_whitespace` was removing them

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,11 +1,29 @@
+local function ensure_eof_newline(lines) end
+
 return {
 	"stevearc/conform.nvim",
 	opts = {
+		formatters = {
+			ensure_eof_newline = {
+				format = function(_, _, lines, callback)
+					-- If the file is empty, just return a single empty line
+					if #lines == 0 then
+						callback(nil, { "" })
+					end
+					-- If the last line is not empty, add an empty line
+					if lines[#lines] ~= "" then
+						table.insert(lines, "")
+					end
+					callback(nil, lines)
+				end,
+			},
+		},
 		formatters_by_ft = {
 			python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 			rust = { "rustfmt" },
 			lua = { "stylua" },
 			ruby = { "rubocop" },
+			json = { "trim_whitespace", "ensure_eof_newline" },
 			["*"] = { "trim_whitespace" },
 		},
 		format_on_save = {
