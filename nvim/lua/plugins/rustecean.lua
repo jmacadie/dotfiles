a0ed80d261869ad641ad456bdc8667ffe2e0d720
James MacAdie <j.macadie@ttg.co.uk>
2025-11-27 17:01:34 +0000

    Move to latest version of rustaceanvim
    This might be unstable and it might be a better idea to stick to ^6 releases but
    I'm going to YOLO it. See you on the other side!

--- nvim/lua/plugins/rustecean.lua
+++ nvim/lua/plugins/rustecean.lua
@@ -2,7 +2,7 @@ local utils = require("plugins.lsp.util")
 
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^5", -- Recommended
+	version = "^7", -- Recommended
 	lazy = false, -- This plugin is already lazy
 	config = function()
 		vim.g.rustaceanvim = {
