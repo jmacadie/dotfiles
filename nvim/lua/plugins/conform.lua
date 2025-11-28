828232de94f0780a75b6fe5f27be8f33ec642e1b
James MacAdie <j.macadie@ttg.co.uk>
2025-11-28 15:21:27 +0000

    Add map to format on the fly
    Because I like it. Although the muscle memory to just save the file is
    strong and not a terrible idea anyway

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,34 +1,42 @@
+---@diagnostic disable-next-line unused-function
 local function ensure_eof_newline(lines) end
 
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		formatters = {
-			ensure_eof_newline = {
-				format = function(_, _, lines, callback)
-					-- If the file is empty, just return a single empty line
-					if #lines == 0 then
-						callback(nil, { "" })
-					end
-					-- If the last line is not empty, add an empty line
-					if lines[#lines] ~= "" then
-						table.insert(lines, "")
-					end
-					callback(nil, lines)
-				end,
+	config = function()
+		local conform = require("conform")
+		local opts = {
+			formatters = {
+				ensure_eof_newline = {
+					format = function(_, _, lines, callback)
+						-- If the file is empty, just return a single empty line
+						if #lines == 0 then
+							callback(nil, { "" })
+						end
+						-- If the last line is not empty, add an empty line
+						if lines[#lines] ~= "" then
+							table.insert(lines, "")
+						end
+						callback(nil, lines)
+					end,
+				},
 			},
-		},
-		formatters_by_ft = {
-			python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-			rust = { "rustfmt" },
-			lua = { "stylua" },
-			ruby = { "rubocop" },
-			json = { "trim_whitespace", "ensure_eof_newline" },
-			["*"] = { "trim_whitespace" },
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+			formatters_by_ft = {
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+				rust = { "rustfmt" },
+				lua = { "stylua" },
+				ruby = { "rubocop" },
+				json = { "trim_whitespace", "ensure_eof_newline" },
+				["*"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		}
+		conform.setup(opts)
+		vim.keymap.set("n", "<leader>f", function()
+			conform.format()
+		end, { desc = "[F] ormat file" })
+	end,
 }
