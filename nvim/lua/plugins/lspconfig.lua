a07c55ac00fbbfdaf271482a91b2d5d7a4d5c723
James MacAdie <j.macadie@ttg.co.uk>
2026-03-05 07:56:02 +0000

    Fix the missing vim keyword in lua
    Not too sure why this has become a problem again but whatevs. I'll
    hardcode in the support as recommended by the lspconfig repo file for
    lua language server

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -56,6 +56,34 @@ return {
 	config = function()
 		vim.lsp.config("lua_ls", {
 			on_attach = utils.on_attach,
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if
+						path ~= vim.fn.stdpath("config")
+						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+					then
+						return
+					end
+				end
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+						path = {
+							"lua/?.lua",
+							"lua/?/init.lua",
+						},
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							"${3rd}/luv/library",
+						},
+					},
+				})
+			end,
 		})
 		vim.lsp.enable("lua_ls")
 
